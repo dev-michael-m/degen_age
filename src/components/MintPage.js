1b50e5867a1640ae7a049fbb0e68122f108431c3
Michael <dev@lythium.io>
2022-07-18 23:38:48 -0500

    working on mint page

--- src/components/MintPage.js
+++ src/components/MintPage.js
@@ -1,17 +1,48 @@
-import React, {useState} from 'react';
-import Logo from '../assets/degen age logo BNW.jpg';
+import React, {useState,useEffect} from 'react';
+import Logo from '../assets/degen age title GNW skull.png';
 import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
 import MinusIcon from '@mui/icons-material/Remove';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import MuiAlert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContentText from '@mui/material/DialogContentText';
+import CheckIcon from '@mui/icons-material/CheckOutlined';
 import '../stylesheet/MintPage.css';
+import { connectWalletSync, Mint } from '../utilities/util';
 
 const MAX_BATCH = 5;
 
 const MintPage = () => {
     const [numMint,setNumMint] = useState(MAX_BATCH);
     const [wallet,setWallet] = useState(null);
+    const [openAlert,setOpenAlert] = useState({
+        open: false,
+        severity: 'success',
+        msg: null
+    });
+    const [dialog,setDialog] = useState(false);
+    const [txn,setTxn] = useState(null);
+
+    useEffect(() => {
+        let mounted = true;
+
+        if(mounted){
+            connectWalletSync().then(res => {
+                setWallet(res.address);
+            }).catch(error => {
+                console.error(error);
+            })
+        }
+
+        return () => {
+            mounted = false;
+        }
+    },[]);
 
     const handleAddMint = () => {
         const _num = document.getElementById('mint-number').value;
@@ -30,19 +61,66 @@ const MintPage = () => {
     }
 
     const handleMint = () => {
-
+        Mint(numMint,wallet).then(res => {
+            setTxn(res.data);
+            setDialog(true);
+        }).catch(error => {
+            console.error(error.msg);
+            setOpenAlert({
+                open: true,
+                msg: error.msg,
+                severity: error.status
+            })
+        })
     }
 
     const handleWalletConnect = () => {
-        
+        connectWalletSync().then(res => {
+            if(res.status){
+                console.log(res.address);
+                setWallet(res.address);
+                setOpenAlert({
+                    open: true,
+                    msg: `Wallet connected!`,
+                    severity: 'success'
+                })
+            }
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+    const toggleSnackbar = () => {
+        setOpenAlert(prevState => ({
+            ...prevState,
+            open: !prevState.open
+        }));
+    }
+
+    const toggleDialog = () => {
+        setDialog(prevState => !prevState);
     }
 
     return (
         <div className='mint-container'>
+            <Snackbar open={openAlert.open} autoHideDuration={6000} anchorOrigin={{vertical: 'top', horizontal: 'center'}} onClose={toggleSnackbar}>
+                <MuiAlert onClose={toggleSnackbar} severity={openAlert.severity}>
+                    {openAlert.msg}
+                </MuiAlert>
+            </Snackbar>
+            <Dialog open={dialog} maxWidth="lg" onClose={toggleDialog}>
+                <DialogTitle className='text-center'>Mint Successful!</DialogTitle>
+                <DialogContent className='text-center' style={{maxWidth: 730}}>
+                    <CheckIcon style={{color: 'green'}} />
+                    <DialogContentText>
+                        You can view your transaction here: {`https://etherscan.io/tx/${txn}`}
+                    </DialogContentText>
+                </DialogContent>
+            </Dialog>
             <div className='mint-wrapper'>
                 <h1>Early Adopters Pass Now Minting!</h1>
                 <div>
-                    <img src={Logo} width={200} height={200}></img>
+                    <img src={Logo} width={300} height={300}></img>
                 </div>
                 <div style={{width: 250}} className='flex-just-even flex-align-center'>
                     <div>
--- src/utilities/util.js
+++ src/utilities/util.js
@@ -1,3 +1,114 @@
+const {ethers} = require('ethers');
+const {whitelist} = require('../whitelist.json');
+
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+const contractABI = require('../contract-abi.json');
+
+const DECIMALS = 3;
+
+export const _contract = new web3.eth.Contract(contractABI, CONTRACT_ADDRESS)
+
 export const FormatNumber = (_num) => {
     return _num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export const MaskAddress = (full_address = '') => {
+    try {
+        const first_half = full_address.substring(0,5);
+        const second_half = full_address.substring(full_address.length - 4);
+        return `${first_half}...${second_half}`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const connectWalletSync = () => {
+    return new Promise(async(resolve,reject) => {
+        if(window.ethereum.request({method: 'eth_requestAccounts'})){
+            try {
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                const signer = provider.getSigner();
+                const address = await signer.getAddress();
+    
+                resolve({status: true, address: address});
+            } catch (error) {
+                console.error(error);
+                reject({status: false, msg: error});
+            }
+        }else{
+            console.warn('users wallet is not connected');
+            reject({status: false, msg: 'users wallet is not connected'});
+        }
+    })    
+}
+
+export const Mint = async (_num, _address) => {
+    return new Promise(async(resolve,reject) => {
+        try {
+            if(_address){
+                const price = await _contract.methods.SALE_PRICE().call();
+                const mint_price = parseFloat(web3.utils.fromWei(price,'ether'));
+                const _saleState = await _contract.methods.sale_state().call();
+                const max_supply = 3000;
+
+                if(_saleState == 1){
+                    const exists = whitelist.find(doc => doc.toUpperCase() == _address.toUpperCase());   // check address is on wl
+
+                    if(exists){
+                        const message = web3.eth.abi.encodeParameters(["address","uint256"],[_address,max_supply]);
+                        const {signature} = web3.eth.accounts.sign(message,PRIVATE_KEY);
+                       
+                        const tx = {
+                            from: _address,
+                            to: process.env.REACT_APP_CONTRACT_ADDRESS,
+                            value: web3.utils.toHex(web3.utils.toWei(String((mint_price * _num).toFixed(DECIMALS)),'ether')),
+                            data: _contract.methods.presale(signature,_num).encodeABI(),
+                        }
+
+                        const txHash = await window.ethereum.request({
+                            method: 'eth_sendTransaction',
+                            params: [tx]
+                        })
+
+                        resolve({data: txHash});
+                    }else{
+                      reject({msg: `You are not currently on the whitelist.`, status: 'warning'})  
+                    }
+                }else if(_saleState == 2){
+                    const tx = {
+                        from: _address,
+                        to: process.env.REACT_APP_CONTRACT_ADDRESS,
+                        value: web3.utils.toHex(web3.utils.toWei(String((mint_price * _num).toFixed(DECIMALS)),'ether')),
+                        data: _contract.methods.pubMint(_num).encodeABI(),
+                    }
+
+                    const txHash = await window.ethereum.request({
+                        method: 'eth_sendTransaction',
+                        params: [tx]
+                    })
+
+                    resolve({data: txHash});
+                }else{
+                    reject({msg: `Sale is currently inactive.`, status: 'warning'})
+                }                
+            }else{
+                reject({
+                    status: 'warning',
+                    msg: `You must connect your MetaMask wallet to continue.  If you don't have a Metamask wallet, follow this link to get started LINK`
+                })
+            }    
+        } catch (error) {
+            console.error(`util.mintNFT: ${error}`)
+            reject({
+                status: 'error',
+                msg: error.message
+            })
+        }
+    })
+    
+  }
